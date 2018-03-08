2507e555de84b7aac0831861eab0ee92518df500
markwang2764 <276410287@qq.com>
2018-03-08 10:29:53 +0800

    feat: apidoc

--- app/routes/mysql-admin.js
+++ app/routes/mysql-admin.js
@@ -11,6 +11,19 @@ import { insertUserData,findDataByName } from '../service/mysql-admin-service';
 
 @Controller('/admin')
 export default class AdminRouter {
+ /**
+ * @apiGroup admin
+ * @api {post} /admin/register
+ * @apiParam {String} name  用户名
+ * @apiParam {String} password     用户密码
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "data": "1",
+ *       "success": true,
+ *       "msg": "注册成功" 
+ *     }
+ */
   @Post('/register')
   @Required({
     body: ['name', 'password']
@@ -21,7 +34,20 @@ export default class AdminRouter {
     console.log(result)
     return (ctx.body = result)
   }
-  
+ /**
+ * @apiGroup admin
+ * @api {post} /admin/login
+ * @apiParam {String} name  用户名
+ * @apiParam {String} password     用户密码
+ *  @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "data": "1",
+ *       "success": true,
+ *       "msg": "登录成功",
+ *       "token": 'dsajkdhskadsa'
+ *     }
+ */
   @Post('/login')
   @Required({
     body: ['name', 'password']
